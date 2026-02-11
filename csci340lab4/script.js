8864931c0b11ab57058d882506499ee305a95f10
KingRM <KingRM@hendrix.edu>
2026-02-10 23:44:29 -0600

    API Test 2

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-    $.ajax({
-        dataType: "html",
-        url: "https://nekos.best/api/v2/neko",
-        success: function(results) {
-            if(results["url"].endsWith(".gif")) {
-                $(".cat").attr("src","images/blank.png");
-            } else {
-                $(".cat").attr("src", results["url"]);
-            }
-        },
-        error: function(xhr,status,error) {
-            console.log(error);
-        }
+    $("#switch-cat").click(function() {
+        $.ajax({
+            dataType: "json",
+            url: "https://random.dog/woof.json",
+            success: function(results) {
+                console.log(results["url"]);
+                if (results["url"].endsWith(".mp4")) {
+                 $('#dog').attr("src", "images/blank.png");
+                } else {
+                    $('#dog').attr("src", results["url"]);
+                }
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+        })
     })
 })
