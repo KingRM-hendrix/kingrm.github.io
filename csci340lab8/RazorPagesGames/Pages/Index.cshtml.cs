c2102dc77f6ba8e27bcde7573887e0489dfda93e
KingRM <KingRM@hendrix.edu>
2026-03-11 22:21:26 -0500

    Lab 8 Step 2 Start

---
+++ csci340lab8/RazorPagesGames/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesGames.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
